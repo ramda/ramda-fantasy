d90ebce366bfa8291bea9f1a9f2d0162c6dee0cb
Chet Harrison <chet.harrison@gmail.com>
2015-05-22 10:55:57 -0700

    fixed a typo
    Not a big deal but I think the we had a little copy/past action here. liftA3 was called liftA2.

--- src/lift3.js
+++ src/lift3.js
@@ -1,5 +1,5 @@
 var R = require('ramda');
 
-module.exports = R.curryN(4, function liftA2(f, a1, a2, a3) {
+module.exports = R.curryN(4, function liftA3(f, a1, a2, a3) {
   return a1.map(f).ap(a2).ap(a3);
 });
