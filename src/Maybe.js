71b3e100887e07e55466f210617687333a754715
buzzdecafe <m_hur@yahoo.com>
2016-02-21 11:10:13 -0500

    fixes #100; rename _Just, _Nothing to Just, Nothing resp.

--- src/Maybe.js
+++ src/Maybe.js
@@ -6,28 +6,28 @@ function Maybe(x) {
   return x == null ? _nothing : Maybe.Just(x);
 }
 
-function _Just(x) {
+function Just(x) {
   this.value = x;
 }
-util.extend(_Just, Maybe);
+util.extend(Just, Maybe);
 
-_Just.prototype.isJust = true;
-_Just.prototype.isNothing = false;
+Just.prototype.isJust = true;
+Just.prototype.isNothing = false;
 
-function _Nothing() {}
-util.extend(_Nothing, Maybe);
+function Nothing() {}
+util.extend(Nothing, Maybe);
 
-_Nothing.prototype.isNothing = true;
-_Nothing.prototype.isJust = false;
+Nothing.prototype.isNothing = true;
+Nothing.prototype.isJust = false;
 
-var _nothing = new _Nothing();
+var _nothing = new Nothing();
 
 Maybe.Nothing = function() {
   return _nothing;
 };
 
 Maybe.Just = function(x) {
-  return new _Just(x);
+  return new Just(x);
 };
 
 Maybe.of = Maybe.Just;
@@ -49,20 +49,20 @@ Maybe.maybe = R.curry(function(nothingVal, justFn, m) {
 });
 
 // functor
-_Just.prototype.map = function(f) {
+Just.prototype.map = function(f) {
   return this.of(f(this.value));
 };
 
-_Nothing.prototype.map = util.returnThis;
+Nothing.prototype.map = util.returnThis;
 
 // apply
 // takes a Maybe that wraps a function (`app`) and applies its `map`
 // method to this Maybe's value, which must be a function.
-_Just.prototype.ap = function(m) {
+Just.prototype.ap = function(m) {
   return m.map(this.value);
 };
 
-_Nothing.prototype.ap = util.returnThis;
+Nothing.prototype.ap = util.returnThis;
 
 // applicative
 // `of` inherited from `Maybe`
@@ -72,24 +72,24 @@ _Nothing.prototype.ap = util.returnThis;
 //  f must be a function which returns a value
 //  f must return a value of the same Chain
 //  chain must return a value of the same Chain
-_Just.prototype.chain = util.baseMap;
+Just.prototype.chain = util.baseMap;
 
-_Nothing.prototype.chain = util.returnThis;
+Nothing.prototype.chain = util.returnThis;
 
 
 //
-_Just.prototype.datatype = _Just;
+Just.prototype.datatype = Just;
 
-_Nothing.prototype.datatype = _Nothing;
+Nothing.prototype.datatype = Nothing;
 
 // monad
 // A value that implements the Monad specification must also implement the Applicative and Chain specifications.
 // see above.
 
 // equality method to enable testing
-_Just.prototype.equals = util.getEquals(_Just);
+Just.prototype.equals = util.getEquals(Just);
 
-_Nothing.prototype.equals = function(that) {
+Nothing.prototype.equals = function(that) {
   return that === _nothing;
 };
 
@@ -98,30 +98,30 @@ Maybe.prototype.isNothing = function() {
 };
 
 Maybe.prototype.isJust = function() {
-  return this instanceof _Just;
+  return this instanceof Just;
 };
 
-_Just.prototype.getOrElse = function() {
+Just.prototype.getOrElse = function() {
   return this.value;
 };
 
-_Nothing.prototype.getOrElse = function(a) {
+Nothing.prototype.getOrElse = function(a) {
   return a;
 };
 
-_Just.prototype.reduce = function(f, x) {
+Just.prototype.reduce = function(f, x) {
   return f(x, this.value);
 };
 
-_Nothing.prototype.reduce = function(f, x) {
+Nothing.prototype.reduce = function(f, x) {
   return x;
 };
 
-_Just.prototype.toString = function() {
+Just.prototype.toString = function() {
   return 'Maybe.Just(' + R.toString(this.value) + ')';
 };
 
-_Nothing.prototype.toString = function() {
+Nothing.prototype.toString = function() {
   return 'Maybe.Nothing()';
 };
 
